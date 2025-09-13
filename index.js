a93bfe37c85e88f1ea0a5cc47ce1a7fb9ffe987e
Muhammed-2105 <muhammed.shameer.s87@kalvium.community>
2025-09-13 11:42:55 +0530

    Initial commit: Set up Express server with MongoDB connection

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 const port = 3010;
 
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message);
+  });
+
 app.use(express.static('static'));
+app.use(express.json()); // For parsing application/json
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
